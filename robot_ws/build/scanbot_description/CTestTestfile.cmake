89ce3e9872fd981536289540166ba269a9a4c93b
JAndres3870 <jaime.escobar.c@ucb.edu.bo>
2026-05-22 04:59:02 -0400

    Etapa 5.2 crear urdf worsapce

---
+++ robot_ws/build/scanbot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/andres/Documents/Proyecto_G/robot_ws/src/scanbot_description
+# Build directory: /home/andres/Documents/Proyecto_G/robot_ws/build/scanbot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
